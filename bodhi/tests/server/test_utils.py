c0d1590d79be93445831bb3fda16c4a6686a630a
Caleigh Runge-Hottman <caleighhottman@gmail.com>
2017-04-06 15:04:58 -0600

    Checks exit code of process to determine where stderr is logged.
    If exit code is 0, the stderr is logged at info. If it is non-zero, stderr logs messages at error.
    
    fixes #1412

--- bodhi/tests/server/test_utils.py
+++ bodhi/tests/server/test_utils.py
@@ -12,6 +12,9 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+import mock
+import unittest
+from bodhi.server import log
 from bodhi.server.buildsys import setup_buildsystem, teardown_buildsystem
 from bodhi.server.config import config
 from bodhi.server.util import (get_critpath_pkgs, get_nvr, markup,
@@ -70,3 +73,50 @@ class TestUtils(object):
         b1, b2 = sorted_builds([new, old])
         assert b1 == new, b1
         assert b2 == old, b2
+
+
+class TestCMDFunctions(unittest.TestCase):
+    @mock.patch('subprocess.Popen')
+    def test_stderr(self, mock_subproc_popen):
+        """
+        Ensures proper behavior when there is err output and the exit code isn't 0.
+        """
+        process_mock = mock.Mock()
+        attrs = {'communicate.return_value': ('output', 'error'), 'returncode': 1}
+        process_mock.configure_mock(**attrs)
+        mock_subproc_popen.return_value = process_mock
+        result = cmd('')
+        self.assertTrue(mock_subproc_popen.called)
+        self.assertEqual(result[0], 'output')
+        self.assertEqual(result[1], 'error')
+        self.assertEqual(result[2], 1)
+
+    @mock.patch('subprocess.Popen')
+    def test_no_err(self, mock_subproc_popen):
+        """
+        Ensures proper behavior when there is no err output and the exit code is 0.
+        """
+        process_mock = mock.Mock()
+        attrs = {'communicate.return_value': ('output', None), 'returncode': 0}
+        process_mock.configure_mock(**attrs)
+        mock_subproc_popen.return_value = process_mock
+        result = cmd('')
+        self.assertTrue(mock_subproc_popen.called)
+        self.assertEqual(result[0], 'output')
+        self.assertEqual(result[1], None)
+        self.assertEqual(result[2], 0)
+
+    @mock.patch('subprocess.Popen')
+    def test_std_err_zero_return_code(self, mock_subproc_popen):
+        """
+        Ensures proper behavior when there is err output, but the exit code is 0.
+        """
+        process_mock = mock.Mock()
+        attrs = {'communicate.return_value': ('output', 'error'), 'returncode': 0}
+        process_mock.configure_mock(**attrs)
+        mock_subproc_popen.return_value = process_mock
+        result = cmd('')
+        self.assertTrue(mock_subproc_popen.called)
+        self.assertEqual(result[0], 'output')
+        self.assertEqual(result[1], 'error')
+        self.assertEqual(result[2], 0)
